54f645de1facd3ac9ade06410866e4889b1c80e5
cjmatheka <cjmatheka@gmail.com>
2024-02-27 17:57:05 +0300

    Add Tsask 1

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+if ARGV.length != 1
+  puts "Usage: ./1-repetition_token_0.rb <text>"
+  exit(1)
+end
+
+text = ARGV[0]
+regex = /hbtn(t*)/
+
+if text =~ regex
+  puts text
+end
