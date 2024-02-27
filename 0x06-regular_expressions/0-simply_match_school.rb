aac90249ac8a7d577f3f1776f65bb9c287276660
cjmatheka <cjmatheka@gmail.com>
2024-02-27 17:32:33 +0300

    Update Task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,1 +1,18 @@
-/School/g
+#!/usr/bin/env ruby
+
+# Ensure we have the required command-line argument
+if ARGV.length != 1
+  puts "Usage: ./0-simply_match_school.rb <text>"
+  exit(1)
+end
+
+# Retrieve the text to search from the command-line
+text_to_search = ARGV[0]
+
+# The simple regular expression to match the word "School"
+regex = /School/
+
+# Check for a match within the provided text
+if text_to_search =~ regex
+  puts text_to_search
+end
