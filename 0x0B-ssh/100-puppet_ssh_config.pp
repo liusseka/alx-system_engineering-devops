d53025eb91f67be3c2e023d726f61e76c631e45e
cjmatheka <cjmatheka@gmail.com>
2024-04-21 21:32:03 +0300

    Update Advanced Task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,16 +4,18 @@
 
 include stdlib
 
+# this code block disables password authentication
 file_line { 'Turn off passwd auth':
   ensure  => present,
-  line    => 'PasswordAuthentication no',
+  line    => '    PasswordAuthentication no',
   path    => '/etc/ssh/ssh_config',
   replace => true,
 }
 
+# this block sets identity for ssh
 file_line { 'Declare identity file':
   ensure  => present,
-  line    => 'IdentityFile ~/.ssh/school',
+  line    => '    IdentityFile ~/.ssh/school',
   path    => '/etc/ssh/ssh_config',
   replace => true,
 }
