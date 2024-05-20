49f9faaed3f51ce240aa1e84853bb16bef6e00b0
cjmatheka <cjmatheka@gmail.com>
2024-05-21 02:21:03 +0300

    Add Task 2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Exports employee To Do List to .csv"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + f"users/{user_id}").json()
+    username = user.get(f"username")
+    todos = requests.get(url + f"todos", params={f"userId": user_id}).json()
+
+    with open(f"{user_id}.csv", f"w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get(f"completed"), t.get(f"title")]
+         ) for t in todos]
