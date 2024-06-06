121e2926af1b545b56106fba8e2a3a2b78bc262b
cjmatheka <cjmatheka@gmail.com>
2024-06-06 09:06:15 +0300

    Update task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""The Script Does the Following:
- + Queries the Reddit API
- + Returns the number of subscribers for a given subreddit.
- + Returns 0 If an invalid subreddit is given. 
+"""
+The Script that Queries a Reddit API
 """
 
 import requests
 
 def number_of_subscribers(subreddit):
-	"""Returns number of subcribers"""
+	"""
+	Returns number of subcribers
+
+	Args: takes a subreddit title
+
+	Returns: Number of Subscribers
+	"""
 	if subreddit is None or type(subreddit) if not str:
 		return 0
 
-	# defines url and parameters
 	url = f'https://www.reddit.com/r/{subreddit}/about.json'
 	headers = {
 	"User-Agent": "SubredditSubscriberCounter/1.0"
 	}
 
-	# Requests and returns the data
 	try:
 		res = requests.get(url, headers=headers, allow_redirects=False)
 		data = res.json()
