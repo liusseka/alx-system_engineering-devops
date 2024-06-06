084e7c631dc190140d1ec839dade01a5202cc8d1
cjmatheka <cjmatheka@gmail.com>
2024-06-06 10:13:36 +0300

    Update Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,6 +11,11 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:v1.0.0"
+    }
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         response.raise_for_status()
