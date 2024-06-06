2ff8f18bf0214a0bf39c0cc29362242fe96681bb
cjmatheka <cjmatheka@gmail.com>
2024-06-06 10:08:03 +0300

    Update Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,19 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    r = requests.get(f'http://www.reddit.com/r/{subreddit}/about.json',
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/julius_charles)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data.get("kind") == "t5":
+            return data["data"]["subscribers"]
+        else:
+            return 0
+
+    except requests.exceptions.RequestException:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/Julius_Charles)"
+v1.0.0 (by /u/julius_charles)"
     }
     params = {
         "limit": 10
