8e9a54a62bc6330ccb312a8a5fc0c4eaba4fad89
liusseka <cjmatheka@gmail.com>
2024-08-22 21:25:46 +0300

    Add Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# This script inceases the limit of the requests that can be served by the nginx server
+
+# Increase the ULIMIT
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
