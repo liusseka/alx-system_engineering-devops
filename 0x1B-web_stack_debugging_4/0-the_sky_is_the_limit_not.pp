839977761d788d445145b29b1149b763871f7ba0
liusseka <cjmatheka@gmail.com>
2024-08-22 21:39:00 +0300

    Make corrections to task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,16 @@
 # This script inceases the limit of the requests that
 # can be served by the nginx server
 
-# Increase the ULIMIT
+# This section increase the ULIMIT
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  # This line modifies the ulimit value
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  # This line specifies the path for the sed command
   path    => '/usr/local/bin/:/bin/'
 } ->
 
-# Restart Nginx
+# This sectionestart Nginx
 exec { 'nginx-restart':
-  command => 'nginx restart',
+  command => '/etc/init.d/nginx restart',
   path    => '/etc/init.d/'
 }
